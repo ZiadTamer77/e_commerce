5c96618dbd54e160af767d837ed7bc5c5d3cc253
ZiadTamer77 <ziademam7798@gmail.com>
2025-08-18 20:30:51 +0300

    Add project files

--- store/models.py
+++ store/models.py
@@ -34,7 +34,7 @@ class Customer(models.Model):
         (MEMBERSHIP_GOLD, "Gold"),
     ]
     membership = models.CharField(max_length=10, choices=MEMBERSHIP_CHOICES)
-    first_name = models.CharField(max_length=255)
+    given_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=255)
