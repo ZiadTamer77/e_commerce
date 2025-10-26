cf5a82abb5bd763f0a78b1218e2c086863994b38
ZiadTamer77 <ziademam7798@gmail.com>
2025-10-26 17:32:08 +0000

    preparing for production v1

--- storefront/urls.py
+++ storefront/urls.py
@@ -26,6 +26,7 @@ admin.site.index_title = "Storefront Admin Portal"
 
 
 urlpatterns = [
+    path("", include("core.urls")),
     path("admin/", admin.site.urls),
     path("playground/", include("playground.urls")),
     path("store/", include("store.urls")),
