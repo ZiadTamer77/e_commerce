db61d9969e4081f28c39ac5c4a9dd56c7ac0ac2b
ZiadTamer77 <ziademam7798@gmail.com>
2025-10-26 17:22:10 +0000

    caching v2

--- playground/views.py
+++ playground/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from django.views.decorators.cache import cache_page
+from django.utils.decorators import method_decorator
+from rest_framework.views import APIView
 import requests
 
 
-def say_hello(request):
-    requests.get("https://httpbin.org/delay/2")
-    return render(request, "hello.html", {"name": "Zeyad"})
+class HelloView(APIView):
+    @method_decorator(cache_page(5 * 60))
+    def get(request, self):
+        response = requests.get("https://httpbin.org/delay/2")
+        data = response.json()
+        return render(request, "hello.html", {"name": "zeyad"})
